53bb83cd012cad77c1127aa81b8f208dd2956daa
anthonysof <antonis.sofras@gmail.com>
2021-12-09 20:37:36 +0200

    server file struct

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+//middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 8081);
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: "Hello World!"
+    });
+});
+
+
+
